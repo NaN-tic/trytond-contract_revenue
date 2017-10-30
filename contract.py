ba0e33cc7f2055e5d64b462b56b40a2f7fc62113
Raimon Esteve <raimon@nan-tic.com>
2017-10-30 13:41:30 +0100

    Set currency digits in revenue and cost fields task-032532

--- contract.py
+++ contract.py
@@ -23,9 +23,6 @@ class Contract:
     cost = fields.Function(fields.Numeric('Cost',
             digits=(16, Eval('currency_digits', 2)),
             depends=['currency_digits']), 'get_total')
-    currency_digits = fields.Function(fields.Integer('Currency Digits'),
-        'on_change_with_currency_digits')
-
 
     @classmethod
     def get_total(cls, contracts, names):
@@ -40,24 +37,7 @@ class Contract:
                     values['cost'][contract.id] += line.cost or Decimal(0)
                 if 'revenue' in names:
                     values['revenue'][contract.id] += line.revenue
-
         return values
-
-
-    @fields.depends('company')
-    def on_change_with_currency_digits(self, name=None):
-        if self.company:
-            return self.company.currency.digits
-        return 2
-
-    @staticmethod
-    def default_currency_digits():
-        Company = Pool().get('company.company')
-        company = Transaction().context.get('company')
-        if company:
-            company = Company(company)
-            return company.currency.digits
-        return 2
 
 
 class ContractLine:
@@ -65,28 +45,11 @@ class ContractLine:
     __metaclass__ = PoolMeta
 
     revenue = fields.Function(fields.Numeric('Revenue',
-            digits=(16, Eval('currency_digits', 2)),
+            digits=(16, Eval('_parent_contract', {}).get('currency_digits', 2)),
             depends=['currency_digits']), 'get_cost_and_revenue')
     cost = fields.Function(fields.Numeric('Cost',
-            digits=(16, Eval('currency_digits', 2)),
+            digits=(16, Eval('_parent_contract', {}).get('currency_digits', 2)),
             depends=['currency_digits']), 'get_cost_and_revenue')
-    currency_digits = fields.Function(fields.Integer('Currency Digits'),
-        'on_change_with_currency_digits')
-
-    @staticmethod
-    def default_currency_digits():
-        Company = Pool().get('company.company')
-        company = Transaction().context.get('company')
-        if company:
-            company = Company(company)
-            return company.currency.digits
-        return 2
-
-    @fields.depends('company')
-    def on_change_with_currency_digits(self, name=None):
-        if self.company:
-            return self.company.currency.digits
-        return 2
 
     @classmethod
     def get_cost_and_revenue(cls, lines, names):
@@ -98,6 +61,8 @@ class ContractLine:
         consumption = Consumption.__table__()
         invoice_line = InvoiceLine.__table__()
         line_ids = [x.id for x in lines]
+        currency_digits = lines and lines[0].contract.currency_digits or 2
+
         values = {}
         for name in names:
             values[name] = dict.fromkeys(line_ids, Decimal(0))
@@ -122,7 +87,9 @@ class ContractLine:
             )
         cursor.execute(*query)
         res = cursor.fetchall()
+
         for line, amount in res:
             if 'revenue' in names:
-                values['revenue'][line] = Decimal(str(amount or 0.0))
+                values['revenue'][line] = Decimal(str(amount or 0.0)).quantize(
+                    Decimal(str(10 ** - currency_digits )))
         return values
